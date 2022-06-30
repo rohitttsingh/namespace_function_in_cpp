c379d65cd794706ce81be0a9680c27405b345d71
Rohit Singh <73903627+rohitttsingh@users.noreply.github.com>
2022-06-30 12:46:26 +0530

    Create program.cpp

---
+++ program.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+namespace first_space
+{
+void func()
+{
+	cout << "Inside first_space" << endl;
+}
+}
+
+namespace second_space
+{
+void func()
+{
+	cout << "Inside second_space" << endl;
+}
+}
+
+using namespace first_space;
+
+int main ()
+{
+
+func();	
+second_space::func();
+first_space::func();
+
+return 0;
+}
